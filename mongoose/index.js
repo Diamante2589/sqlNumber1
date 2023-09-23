9eea734189678ea2fc4918529b3dcd277ceb2c8f
neberpa <nestorebernalpardo@gmail.com>
2023-09-23 12:19:18 -0500

    adicionar carpeta

---
+++ mongoose/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const server = express();
+const PORT = 3000;
+
+server.use(
+  express.json()
+); /* transformar el cuerpo de la peticion en un json */
+server.use(
+  cors()
+); /* me permite recibir solicitudes de clientes fuera de mi dominio */
+
+async function main() {
+  await mongoose.connect("mongodb+srv://sensei:1234@cluster0.vep1mp4.mongodb.net/");
+
+  server.listen(PORT, () => {
+    console.log(`Server listen in http://localhost:${PORT}`);
+  });
+}
+
+main();
